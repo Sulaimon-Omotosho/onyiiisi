bce6270c928bf67eb0b38ce087b68c69d6a55733
Sulaimon Omotosho <susudef2u@gmail.com>
2024-03-26 13:05:50 +0100

    Chore: Bug fix, comment out isAdmin

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -64,18 +64,18 @@ export const authOptions: NextAuthOptions = {
   debug: process.env.NODE_ENV === 'development',
 }
 
-export async function isAdmin() {
-  const session = await getServerSession()
-  const userEmail = session?.user?.email
-  if (!userEmail) {
-    return false
-  }
-  const userInfo = await User.findOne({ email: userEmail })
-  if (!userInfo) {
-    return false
-  }
-  return userInfo?.is_admin
-}
+// export async function isAdmin() {
+//   const session = await getServerSession()
+//   const userEmail = session?.user?.email
+//   if (!userEmail) {
+//     return false
+//   }
+//   const userInfo = await User.findOne({ email: userEmail })
+//   if (!userInfo) {
+//     return false
+//   }
+//   return userInfo?.is_admin
+// }
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
